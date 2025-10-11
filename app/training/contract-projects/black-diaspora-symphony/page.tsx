49342fa554e6d5258a77475ea3c9ad2ded0f9a9b
Ezra Haugabrooks <ezra@aymscores.com>
2025-10-11 18:34:44 -0500

    small changes to test deployment to vercel

--- app/training/contract-projects/black-diaspora-symphony/page.tsx
+++ app/training/contract-projects/black-diaspora-symphony/page.tsx
@@ -175,7 +175,7 @@ export default function BlackDiasporaSymphonyPage() {
                 </div>
                 <div className="flex items-center text-white">
                   <Location className="w-5 h-5 mr-3 text-purple-400" />
-                  <span>Orlando Concert Hall, Florida</span>
+                  <span>Central United Methodist Church, Wisconsin</span>
                 </div>
               </div>
 
